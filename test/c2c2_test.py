cd318f83329c23deca01ac454d1a8dbdc52014e8
Hiroya-W <hyuyukun@gmail.com>
2024-02-16 19:50:32 +0900

    format

--- test/c2c2_test.py
+++ test/c2c2_test.py
@@ -1,50 +1,62 @@
-import os
 import glob
 import json
+import os
 import subprocess
-import pytest
 import sys
 from pathlib import Path
+
+import pytest
+
 
 class Casl2AssembleError(Exception):
     pass
 
+
 def command(cmd):
     try:
-        result = subprocess.run(cmd, shell=True, check=True,
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                universal_newlines=True)
+        result = subprocess.run(
+            cmd,
+            shell=True,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True,
+        )
         for line in result.stdout.splitlines():
             yield line
     except subprocess.CalledProcessError:
-        print('外部プログラムの実行に失敗しました [' + cmd + ']', file=sys.stderr)
+        print("外部プログラムの実行に失敗しました [" + cmd + "]", file=sys.stderr)
         sys.exit(1)
+
 
 def common_task(casl2_file, out_file):
     try:
         c2c2 = Path(__file__).parent.parent.joinpath("c2c2.js")
-        assembler_text = command("node {} -n -c -a {}".format(c2c2,casl2_file))
+        assembler_text = command("node {} -n -c -a {}".format(c2c2, casl2_file))
         if not "DEFINED SYMBOLS" in assembler_text:
             raise Casl2AssembleError
         with open("input.json") as fp:
             input = json.load(fp)
-        inputparams = ''
+        inputparams = ""
         if Path(casl2_file).name in input.keys():
-            inputparams = ' '.join([i for i in input[Path(casl2_file).name]])
-        terminal_text = command("node {} -n -q -r {} {}".format(c2c2,casl2_file,inputparams))
-        with open(out_file, mode='w') as fp:
+            inputparams = " ".join([i for i in input[Path(casl2_file).name]])
+        terminal_text = command(
+            "node {} -n -q -r {} {}".format(c2c2, casl2_file, inputparams)
+        )
+        with open(out_file, mode="w") as fp:
             for line in terminal_text:
-                fp.write(line + '\n')
+                fp.write(line + "\n")
     except Casl2AssembleError:
-        with open(out_file, mode='w') as fp:
+        with open(out_file, mode="w") as fp:
             fp.write("============ASSEMBLE ERROR==============\n")
             for line in assembler_text:
-                fp.write(line + '\n')
+                fp.write(line + "\n")
         raise Casl2AssembleError
     except Exception as err:
-        with open(out_file, mode='w') as fp:
+        with open(out_file, mode="w") as fp:
             print(err, file=fp)
         raise err
+
 
 # ===================================
 # pytest code
@@ -55,6 +67,7 @@ TEST_EXPECT_DIR = "test_expects"
 
 sample_files = sorted(glob.glob("samples/**/*.cas", recursive=True))
 test_data = sample_files
+
 
 @pytest.mark.timeout(20)
 @pytest.mark.parametrize(("casl2_file"), test_data)
--- test/terminal_casl2comet2_test.py
+++ test/terminal_casl2comet2_test.py
@@ -1,25 +1,24 @@
-import os
 import glob
-import re
-import json
 import itertools
-import pytest
+import json
+import os
+import re
 from pathlib import Path
+
+import pytest
 from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
-from selenium.webdriver import ChromeOptions
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver import ActionChains, ChromeOptions, FirefoxOptions, Keys
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
-from selenium.webdriver import Keys, ActionChains
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.common.exceptions import TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
-
 DRIVER_DESTINATION_PATH = "./drivers"
+
 
 class Casl2AssembleError(Exception):
     pass
@@ -29,7 +28,12 @@ def init_firefox_driver():
     options = FirefoxOptions()
     options.add_argument("-headless")
     options.set_preference("accessibility.typeaheadfind.manual", False)
-    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager(path=DRIVER_DESTINATION_PATH).install()), options=options)
+    driver = webdriver.Firefox(
+        service=FirefoxService(
+            GeckoDriverManager(path=DRIVER_DESTINATION_PATH).install()
+        ),
+        options=options,
+    )
     return driver
 
 
@@ -37,13 +41,18 @@ def init_chrome_driver():
     options = ChromeOptions()
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--allow-running-insecure-content')
-    options.add_argument('--disable-web-security')
-    options.add_argument('--disable-desktop-notifications')
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--allow-running-insecure-content")
+    options.add_argument("--disable-web-security")
+    options.add_argument("--disable-desktop-notifications")
     options.add_argument("--disable-extensions")
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager(path=DRIVER_DESTINATION_PATH).install()), options=options)
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    driver = webdriver.Chrome(
+        service=ChromeService(
+            ChromeDriverManager(path=DRIVER_DESTINATION_PATH).install()
+        ),
+        options=options,
+    )
     return driver
 
 
@@ -54,56 +63,62 @@ def common_task(driver, casl2_file, out_file, timeout):
     try:
         e = driver.find_element(By.ID, "casl2src")
         e.clear()
-        buf_to_js = buf.replace('\\', '\\\\').replace('\n', '\\n').replace('\"', '\\"')
-        driver.execute_script(f'document.getElementById("casl2src").value="%s"' % buf_to_js)
+        buf_to_js = buf.replace("\\", "\\\\").replace("\n", "\\n").replace('"', '\\"')
+        driver.execute_script(
+            f'document.getElementById("casl2src").value="%s"' % buf_to_js
+        )
         # e.send_keys(buf)
         e = driver.find_element(By.ID, "assemble")
         e.click()
         e = driver.find_element(By.ID, "terminal_casl2")
-        if not "DEFINED SYMBOLS" in e.text:
+        if "DEFINED SYMBOLS" not in e.text:
             raise Casl2AssembleError
         e = driver.find_element(By.ID, "terminal_comet2")
-        ActionChains(driver)\
-            .move_to_element(e)\
-            .click()\
-            .send_keys("run")\
-            .send_keys(Keys.RETURN)\
-            .perform()
+        ActionChains(driver).move_to_element(e).click().send_keys("run").send_keys(
+            Keys.RETURN
+        ).perform()
         with open("input.json") as fp:
             input = json.load(fp)
         if Path(casl2_file).name in input.keys():
             for in_str in input[Path(casl2_file).name]:
-                ActionChains(driver)\
-                    .move_to_element(e)\
-                    .click()\
-                    .send_keys(in_str)\
-                    .send_keys(Keys.RETURN)\
-                    .perform()
-        WebDriverWait(driver, timeout).until(expected_conditions.text_to_be_present_in_element((By.ID, "terminal_comet2"), "Program finished"))
+                ActionChains(driver).move_to_element(e).click().send_keys(
+                    in_str
+                ).send_keys(Keys.RETURN).perform()
+        WebDriverWait(driver, timeout).until(
+            expected_conditions.text_to_be_present_in_element(
+                (By.ID, "terminal_comet2"), "Program finished"
+            )
+        )
         terminal_text = e.text
-        with open(out_file, mode='w') as fp:
-            out_match = re.search(r"^comet2> run\n(.*)Program finished", terminal_text, flags=re.MULTILINE | re.DOTALL)
+        with open(out_file, mode="w") as fp:
+            out_match = re.search(
+                r"^comet2> run\n(.*)Program finished",
+                terminal_text,
+                flags=re.MULTILINE | re.DOTALL,
+            )
             if out_match:
                 o_raw = out_match.group(1).splitlines(True)
-                for l in o_raw:
-                    if (re.match(r"(IN|OUT)>", l)):
-                        fp.write(l)
-                #fp.write(out_match.group(1))
+                for line in o_raw:
+                    if re.match(r"(IN|OUT)>", line):
+                        fp.write(line)
+                # fp.write(out_match.group(1))
     except TimeoutException:
         terminal_text = e.text
-        with open(out_file, mode='w') as fp:
+        with open(out_file, mode="w") as fp:
             fp.write("============TIMEOUT==============\n")
-            out_match = re.search(r"^run\n(.*)", terminal_text, flags=re.MULTILINE | re.DOTALL)
+            out_match = re.search(
+                r"^run\n(.*)", terminal_text, flags=re.MULTILINE | re.DOTALL
+            )
             if out_match:
                 fp.write(out_match.group(1))
         raise TimeoutException(f"Timeout ({timeout}sec)")
     except Casl2AssembleError:
-        with open(out_file, mode='w') as fp:
+        with open(out_file, mode="w") as fp:
             fp.write("============ASSEMBLE ERROR==============\n")
             fp.write(e.text)
         raise Casl2AssembleError
     except Exception as err:
-        with open(out_file, mode='w') as fp:
+        with open(out_file, mode="w") as fp:
             print(err, file=fp)
         raise err
 
@@ -133,6 +148,7 @@ def Chrome():
     yield driver
     driver.quit()
 
+
 @pytest.mark.parametrize(("driver_name,casl2_file"), test_data)
 def test_casl2comet2_run(driver_name, casl2_file, request):
     driver = request.getfixturevalue(driver_name)
@@ -142,7 +158,7 @@ def test_casl2comet2_run(driver_name, casl2_file, request):
     if not Path(TEST_RESULT_DIR).exists():
         os.mkdir(TEST_RESULT_DIR)
     out_file = Path(TEST_RESULT_DIR).joinpath(Path(casl2_file).name + ".out")
-    if (Path(casl2_file).name == "sample16.cas"):
+    if Path(casl2_file).name == "sample16.cas":
         timeout = 90
     else:
         timeout = 10
